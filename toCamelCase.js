12a8809cbec5602a7c0626067bf3bfa9d33f4031
Ellezique <gizellevanzyl@gmail.com>
2021-06-03 10:00:43 +1000

    completed

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,6 +1,20 @@
 function toCamelCase(str) { 
 //code in here upate
+//regexr.com   Character classes > character set /([A-Z])\w+/g  
+    // () groups multiple tokens together and creates a capture group for extracting a substring or using a backreference
+    // [] character set. Match any character in the set
+    //-,_ is the range. Matches character ot the range. 
+    //remove the + after w otherwise the entire second word will capitalise
+//The replace() method searches a string for a specified value, or a regular expression, and returns a new string where the specified values are replaced.
+//The substr() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
+    return str.replace(/([_, -]\w)/g, function (match) {
+        return match.toUpperCase().substr(1);
+    });
 }
+
+console.log(toCamelCase("foo_bar")) // returns "fooBar" 
+console.log(toCamelCase("Hello-there")) // returns "HelloThere"
+console.log(toCamelCase("")) // returns ""
 
 //this is here to run the tests
 module.exports = {toCamelCase};
