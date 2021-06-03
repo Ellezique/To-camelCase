385fc16a103d191404a70b64eef2e5dbd470f5c7
Ellezique <gizellevanzyl@gmail.com>
2021-06-03 14:25:32 +1000

    improve and finalise with notes

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,14 +1,16 @@
 function toCamelCase(str) { 
 //code in here upate
-//regexr.com   Character classes > character set /([A-Z])\w+/g  
+//regexr.com   Character classes > character set /([A-Z])\w+/g    
     // () groups multiple tokens together and creates a capture group for extracting a substring or using a backreference
     // [] character set. Match any character in the set
     //-,_ is the range. Matches character ot the range. 
-    //remove the + after w otherwise the entire second word will capitalise
+    // \w matches any word character
+    //remove the + after w otherwise the entire second word will capitalise. 
+    // /g global search. Retain the index of the last match, allowing iterative searches
 //The replace() method searches a string for a specified value, or a regular expression, and returns a new string where the specified values are replaced.
 //The substr() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
-    return str.replace(/([_, -]\w)/g, function (match) {
-        return match.toUpperCase().substr(1);
+    return str.replace(/([_, -]\w)/g, function (str) {
+        return str.toUpperCase().substr(1);
     });
 }
 
